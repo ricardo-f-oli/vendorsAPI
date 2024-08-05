a5dbc790144befd5307ab1b3acd2433f6fd7cfd7
Ricardo Oliveira <ricardo.fer.oliveira964@gmail.com>
2024-08-05 13:25:45 -0300

    Starting development

---
+++ src/main/java/com/example/vendorsAPI/VendorsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vendorsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vendorsAPI/VendorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vendorsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
